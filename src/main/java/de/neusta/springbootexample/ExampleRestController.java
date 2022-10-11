24b2e72ae7722e2367282de8c6f7234a47d6195d
KaiRenken <kai.renken@gmx.de>
2022-10-11 11:25:02 +0200

    added post example

---
+++ src/main/java/de/neusta/springbootexample/ExampleRestController.java
@@ -0,0 +1,31 @@
+package de.neusta.springbootexample;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ExampleRestController {
+
+    @GetMapping("/greet/{name}")
+    public String greetWithNameInPathVariable(@PathVariable String name) {
+        return "Moin, " + name + "!";
+    }
+
+    /*
+    An diesen Endpunkt kann ein Gruß geschickt werden, indem die Parameter die das Objekt 'GreetingDto' erwartet (also 'name')
+    in einen JSON-String verpackt und als Body mitgesendet werden. So ein JSON-String sähe dann so aus:
+
+    {
+        "name": "Kai"
+    }
+
+     */
+    @PostMapping("/greet")
+    public String greetWithNameInRequestBody(@RequestBody GreetingDto greetingDto) {
+        return "Moin, " + greetingDto.getName() + "!";
+    }
+}
---
+++ src/main/java/de/neusta/springbootexample/GreetingDto.java
@@ -0,0 +1,15 @@
+package de.neusta.springbootexample;
+
+public class GreetingDto {
+
+    private final String name;
+
+    public GreetingDto(String name) {
+        this.name = name;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+}
